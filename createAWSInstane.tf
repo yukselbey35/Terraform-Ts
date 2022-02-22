f9b1ec840348fd0faacd6d497db406bf515e764d
yukselbey35 <yukselbey35@gmail.com>
2022-02-22 13:47:47 -0700

    Edit createAWSIN file

--- createAWSInstane.tf
+++ createAWSInstane.tf
@@ -1,9 +1,14 @@
 resource "aws_instance" "vm" {
   ami           = "ami-05803413c51f242b7"
   subnet_id     = "subnet-00a45fb5a9aefb9df"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   tags = {
     Name = "my-first-tf-node"
   }
 }
 
+provider "aws" {
+  access_key = "ACCESS_KEY_HERE"
+  secret_key = "SECRET_KEY_HERE"
+
+}
