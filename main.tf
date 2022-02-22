aced108f8fa584991a9bb84a5fa6053a05f7d9ad
yukselbey35 <yukselbey35@gmail.com>
2022-02-22 13:33:43 -0700

    m

--- main.tf
+++ main.tf
@@ -2,9 +2,7 @@
 # Configure the AWS Provider
 provider "aws" {
     version = "4.0.0"
-    access_key = "AKIAZCLAKWXC5DY5M7ER"
-    secret_key = "SECRET_KEY"
-    region     = "us-east-2"
+    region = "us-east-2"
 }
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
